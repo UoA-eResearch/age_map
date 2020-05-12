bd8ae43a89ff8301458737cd8751fc311c2ecf6d
Nick Young <nick.young@auckland.ac.nz>
2020-05-12 14:01:26 +1200

    add smoker data

--- parse_csv.py
+++ parse_csv.py
@@ -12,18 +12,17 @@ for fn in files:
   with open(fn) as f:
     reader = csv.DictReader(f)
     for row in reader:
-      #"Year","Area_type","Area_code","Area_description","Age_group_5_year_groups_to_85_years_and_over_code","Age_group_5_year_groups_to_85_years_and_over_description","Sex_code","Sex_description","Maori_ethnic_group
-#_indicator_summary_code","Maori_ethnic_group_indicator_summary_description","Census_usually_resident_population_count","Age_group_5_year_groups_to_85_years_and_over_by_Maori_ethnic_group_indicator_summary_perc
-#ent","Age_group_5_year_groups_to_85_years_and_over_by_sex_percent"
+#"Year","Area_type","Area_code","Area_description","Cigarette_smoking_behaviour_code","Cigarette_smoking_behaviour_description","Sex_code","Sex_description","Age_group_life_cycle_groups_code","Age_group_life_cycle_groups_description","Maori_ethnic_group_indicator_summary_code","Maori_ethnic_group_indicator_summary_description","Census_usually_resident_population_count_aged_15_years_and_over","Cigarette_smoking_behaviour_by_Maori_ethnic_group_indicator_summary_percent","Cigarette_smoking_behaviour_by_sex_percent","Cigarette_smoking_behaviour_percent"
       if row["Area_type"] == "Statistical Area 2":
         code = row["Area_code"]
         year = row["Year"]
-        age_group = row["Age_group_5_year_groups_to_85_years_and_over_description"]
+        smoker = row["Cigarette_smoking_behaviour_description"]
+        age_group = row["Age_group_life_cycle_groups_description"]
         sex = row["Sex_description"]
         maori = row["Maori_ethnic_group_indicator_summary_description"]
         if maori != "Total":
           continue
-        count = row["Census_usually_resident_population_count"]
+        count = row["Census_usually_resident_population_count_aged_15_years_and_over"]
         if count == "C":
           count = None
         else:
@@ -32,8 +31,14 @@ for fn in files:
           o[code] = {
             "Male": {},
             "Female": {},
-            "Total": {}
+            "Total": 0
           }
-        o[code][sex][age_group] = count
+        if smoker == "Total stated" and sex == "Total" and age_group == "Total" and count:
+          o[code]["Total"] += count
+        if smoker in ["Total", "Total stated", "Not elsewhere included"] or age_group in ["Under 15 years", "Total"] or sex == "Total":
+          continue
+        if smoker not in o[code][sex]:
+          o[code][sex][smoker] = {}
+        o[code][sex][smoker][age_group] = count
 
-print(json.dumps(o, sort_keys=True, indent=4))
+print(json.dumps(o, sort_keys=True, indent=2))
